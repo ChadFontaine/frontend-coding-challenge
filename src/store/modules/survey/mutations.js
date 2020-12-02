864b762f021fb2be63489a5222a0fb6f1d46e750
ChadFontaine <rmfloris@gmail.com>
2020-12-02 17:23:55 +0100

    feat: add state management using vuex to the project

--- src/store/modules/survey/mutations.js
+++ src/store/modules/survey/mutations.js
@@ -1,2 +1,38 @@
+import { STATUS, initialState } from './state'
+
 export default {
+  setName (state, name) {
+    state.name = name
+  },
+  addGoal (state, goal) {
+    state.goals = [...state.goals, goal]
+  },
+  removeGoal (state, item) {
+    state.goals = state.goals.filter(goal => goal !== item)
+  },
+  addDiet (state, diet) {
+    state.diet = [...state.diet, diet]
+  },
+  removeDiet (state, item) {
+    state.diet = state.diet.filter(diet => diet !== item)
+  },
+  setDOB (state, item) {
+    state.dob = item
+  },
+  setCurrentStep (state, step) {
+    state.currentStep = step
+  },
+  setSubmissionLoading (state) {
+    state.submissionStatus = STATUS['IN-PROGRESS']
+  },
+  setSubmissionError (state, error) {
+    state.submissionStatus = STATUS.ERROR
+    state.submissionError = error
+  },
+  setSubmissionSuccess (state) {
+    state.submissionStatus = STATUS.OK
+  },
+  resetSurvey (state) {
+    Object.assign(state, initialState)
+  }
 }
