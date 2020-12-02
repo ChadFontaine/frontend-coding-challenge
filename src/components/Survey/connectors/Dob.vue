e29178159a1ccb4e61bc673ef44a52f9d34c5308
ChadFontaine <rmfloris@gmail.com>
2020-12-02 17:24:43 +0100

    feat: improve connectors and add success connector

--- src/components/Survey/connectors/Dob.vue
+++ src/components/Survey/connectors/Dob.vue
@@ -1,51 +1,91 @@
 <script>
-  import ThvButton from '@/components/Shared/Button'
-  import DobInput from '@/components/Shared/DobInput'
+import ThvButton from '@/components/Shared/Button'
+import DobInput from '@/components/Shared/DobInput'
 
-  export default {
-    name: 'Dob',
-    components: {
-      DobInput,
-      ThvButton
+export default {
+  name: 'Dob',
+  components: {
+    DobInput,
+    ThvButton
+  },
+  data () {
+    return {
+      dob: this.$store.getters['survey/dob']
+    }
+  },
+  created () {
+    this.$store.dispatch('survey/setCurrentStep', this.$options.name)
+  },
+  computed: {
+    submissionError () {
+      return this.$store.getters['survey/submissionError']
     },
-    computed: {
-      disableNext () {
-        let under18 = this.$refs.DobInput && this.$refs.DobInput.ageError
-        return this.dob === '' || this.errors.items.length > 0 || under18 === true
-      },
-      feedback () {
-        if (this.$refs.DobInput && this.$refs.DobInput.ageError) {
-          return 'You must be over 18'
+    disableNext () {
+      let under18 = this.$refs.DobInput && this.$refs.DobInput.ageError
+      return (
+        this.dob === '' || this.errors.items.length > 0 || under18 === true
+      )
+    },
+    feedback () {
+      if (this.$refs.DobInput && this.$refs.DobInput.ageError) {
+        return 'You must be over 18'
+      }
+      // give priority to submission errors
+      if (this.submissionError) return this.submissionError
+      return this.errors.items.length > 0 ? this.errors.items[0].msg : ''
+    }
+  },
+  methods: {
+    async handleSurveySubmission () {
+      try {
+        await this.$store.dispatch('survey/sendToApi')
+        this.$router.push('/success')
+      } catch (err) {
+        // error is already handled in the sendToApi action
+      }
+    },
+    submit () {
+      this.$refs.DobInput.handleSubmit()
+      this.$validator.reset()
+      this.$validator.validate().then(result => {
+        if (result && !this.feedback) {
+          this.$store.commit('survey/setDOB', this.dob)
+          this.handleSurveySubmission()
         }
-        return this.errors.items.length > 0 ? this.errors.items[0].msg : ''
-      }
+      })
     },
-    methods: {
-      submit () {
-        this.$refs.DobInput.handleSubmit()
-        this.$validator.reset()
-        this.$validator.validate().then(result => {
-          if (result && !this.feedback) {
-            this.$router.push('/success')
-          }
-        })
-      },
-      back () {
-        this.$router.push('/diet')
-      }
+    back () {
+      this.$router.push('/diet')
     }
   }
+}
 </script>
 
-<template>  
+<template>
   <div class="grid-x grid-x-margin">
     <div class="cell small-12 medium-6 medium-offset-3">
       <div class="survey-questions__dob align-center">
         <h1>How old are you?</h1>
         <div class="spacer sp__top--sm"></div>
-        <p class="body--large question-description">This helps us recommend the best test for you. We know it's a bit forward but our lips are sealed!</p>
+        <p class="body--large question-description">
+          This helps us recommend the best test for you. We know it's a bit
+          forward but our lips are sealed!
+        </p>
         <div class="spacer sp__top--sm"></div>
-        <dob-input class="align-center survey-input" ref="DobInput" v-validate="'required'" data-vv-value-path="dob" :value="dob" name="dob" :error="errors.has('dob')" minAge="18" :feedback="feedback" @keyup.enter="submit" label=""></dob-input>
+        <dob-input
+          class="align-center survey-input"
+          v-model="dob"
+          ref="DobInput"
+          v-validate="'required'"
+          data-vv-value-path="dob"
+          :value="dob"
+          name="dob"
+          :error="errors.has('dob')"
+          minAge="18"
+          :feedback="feedback"
+          @keyup.enter="submit"
+          label=""
+        ></dob-input>
         <div class="grid-x button-container">
           <div class="cell auto">
             <div class="back-button-container">
@@ -53,7 +93,13 @@
             </div>
           </div>
           <div class="cell auto align-right">
-            <thv-button element="button" size="large" @click="submit">Next</thv-button>
+            <thv-button
+              element="button"
+              :disabled="disableNext"
+              size="large"
+              @click="submit"
+              >Next</thv-button
+            >
           </div>
         </div>
       </div>
@@ -62,13 +108,12 @@
 </template>
 
 <style lang="css">
-  .survey-questions__dob .input__dob .input__group .input__dob__day,
-  .survey-questions__dob .input__dob .input__group .input__dob__month,
-  .survey-questions__dob .input__dob .input__group .input__dob__year {
-    max-width: 300px;
-  }
-  .survey-questions__dob .input__feedback {
-    text-align: left;
-  }
-
+.survey-questions__dob .input__dob .input__group .input__dob__day,
+.survey-questions__dob .input__dob .input__group .input__dob__month,
+.survey-questions__dob .input__dob .input__group .input__dob__year {
+  max-width: 300px;
+}
+.survey-questions__dob .input__feedback {
+  text-align: left;
+}
 </style>
--- src/components/Survey/connectors/Success.vue
+++ src/components/Survey/connectors/Success.vue
@@ -1,19 +1,116 @@
 <script>
-  export default {
-    name: 'Success',
-    components: {
+import ThvButton from '@/components/Shared/Button'
+import ThrivalingImg from '@/assets/images/thrivaling.png'
+
+const DATE_FORMAT = { day: 'numeric', month: 'numeric', year: 'numeric' }
+
+export default {
+  name: 'Success',
+  created () {
+    this.$store.dispatch('survey/setCurrentStep', this.$options.name)
+  },
+  components: {
+    ThvButton,
+    ThrivalingImg
+  },
+  methods: {
+    startOver () {
+      this.$store.commit('survey/resetSurvey')
+      this.$router.push('/name')
+    }
+  },
+  computed: {
+    imgSrc () {
+      return ThrivalingImg
     },
-    computed: {
-    },
-    methods: {
+    surveyValues () {
+      return [
+        {
+          name: 'Name',
+          value: this.$store.getters['survey/name']
+        },
+        {
+          name: 'Goals',
+          value: this.$store.getters['survey/goals'].join(', ')
+        },
+        {
+          name: 'Diet',
+          value: this.$store.getters['survey/diet'][0]
+        },
+        {
+          name: 'DOB',
+          value: new Intl.DateTimeFormat('default', DATE_FORMAT).format(new Date(this.$store.getters['survey/dob']))
+        }
+      ]
     }
   }
+}
 </script>
 
 <template>
-  
+  <div class="grid-x grid-x-margin">
+    <div class="cell small-12 medium-6 medium-offset-3">
+      <div class="spacer sp__top--xs"></div>
+      <div class="survey-questions__goals align-center">
+        <img :src="imgSrc" height="150px" />
+        <div class="spacer sp__top--xs"></div>
+        <h1>All done!</h1>
+        <div class="spacer sp__top--xs"></div>
+        <p>
+          Thank you for completing the survey. The following results have been
+          submitted:
+        </p>
+        <div class="spacer sp__top--sm"></div>
+      </div>
+      <section class="summary-card">
+        <ul class="results">
+          <li class="result-item" v-for="n in surveyValues" :key="n.name">
+            <p class="result-label">{{ n.name }}</p>
+            <p class="result-value">{{ n.value }}</p>
+          </li>
+        </ul>
+        <thv-button
+          class="align-center"
+          element="button"
+          size="large"
+          @click="startOver"
+          >Start over</thv-button
+        >
+      </section>
+    </div>
+  </div>
 </template>
 
-<style lang='css'>
-
+<style lang="css">
+.summary-card {
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: #ffffff;
+  box-shadow: 0 0.375rem 1.25rem rgba(61, 66, 80, 0.18);
+}
+.result-label {
+  color: #99a2b5;
+  font-size: 14px;
+  margin-bottom: 2px;
+}
+.result-value {
+  color: #3d4250;
+  font-size: 16px;
+}
+.result-item {
+  margin-bottom: 20px;
+}
+.result-item:last-of-type {
+  margin-bottom: 0px;
+}
+.results {
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
 </style>
